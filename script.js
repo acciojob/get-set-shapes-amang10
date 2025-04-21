b57dcb0b6aa54c4200d87cdf5d159fc837a7c44b
amang10 <90174464+amang10@users.noreply.github.com>
2025-04-21 10:26:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-//complete this code
-class Rectangle {}
+// Creating a Rectangle instance
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
 
-class Square extends Animal {}
-
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+// Creating a Square instance
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
